336e442ed68fd46473c776bcbbba2fa6d0d930ce
ivan.altomera <ivan.altomera@stash.ph>
2023-10-12 00:24:35 +0800

    feat: primitives

---
+++ 2_primitives/Primitives.java
@@ -0,0 +1,15 @@
+public class Primitives {
+    public static void main(String[] args) {
+        int ten; // set to 10
+        System.out.println("10"); // replace to `ten`
+        
+        float tenFloat; // set and cast `ten` to float
+        System.out.println("10.0"); // replace to tenFloat
+
+        double tenDouble; // set and cast `tenFloat` to double 
+        System.out.println("10.0"); // replace to tenDouble
+
+        int fifteen = 5; // set and cast `tenDouble` to int and add 10
+        System.out.println("15"); // replace to fifteen
+    }
+}
---
+++ 2_primitives/README.md
@@ -0,0 +1,24 @@
+# 2. Primitives
+
+Rationale:
+- To be able to understand the importance of data types
+
+Lessons:
+- In order to convert number to any other number types, you can use type cast operator
+```java
+int five = 5;
+float twoPointFive = (float) five / 2.0;
+```
+
+Objectives:
+- Set all of the variables based on the comments
+- Replace all static inputs `System.out.println` with variables
+
+Acceptance Criteria:
+- Results are `System.out.println`ed from variables with the following outputs:
+```
+10
+10.0
+10.0
+15
+```
